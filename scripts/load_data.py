34a1e29259cf8022509b05f37e4ed5c2c340db13
Deep Patel <dmpatel@andrew.cmu.edu>
2023-10-14 21:30:48 -0400

    Modified script to display the lidar data

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,5 +1,6 @@
 # perceptions specific imports
 from data.utils.dataloader import DataLoader
+from predictors.utils.lidar import *
 
 # general python imports
 import time
@@ -9,14 +10,17 @@ import cv2
 def main():
     dl = DataLoader("data/raw/track-testing-09-29")
 
+    window = init_visualizer_window()
+
     for i in range(len(dl)):
         # load the i-th image from track testing run
         data = dl[i]
-        img = data["left_color"]
+        points = data["points"][:, :3]
+        points = points[:, [1,0,2]]
+        points[:,0] = -points[:,0]
 
-        # display the image
-        cv2.imshow(f"left image (idx: {i})", img)
-        cv2.waitKey(0)
+        # display the point cloud
+        update_visualizer_window(window, points)
 
 if __name__ == "__main__":
     main()
