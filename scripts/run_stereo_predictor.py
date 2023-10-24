dabd6637d982f4a96d053cea4958afe3ab9755dc
pakth0 <thomaspalakapilly@gmail.com>
2023-10-24 02:07:26 -0400

    fixed organization and imports
    added to respective folders

--- scripts/run_stereo_predictor.py
+++ scripts/run_stereo_predictor.py
@@ -2,18 +2,15 @@ import sys
 import os
 import numpy as np
 
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(SCRIPT_DIR))
-
-from predictors.StereoPredictor.StereoPredictor import StereoPredictor
-from data.utils.dataloader import DataLoader
+from perc22a.predictors.StereoPredictor.StereoPredictor import StereoPredictor
+from perc22a.data.utils.dataloader import DataLoader
 
 
 import time
 import cv2
 
 def main():
-    predictor = StereoPredictor('ultralytics/yolov5', 'predictors/StereoPredictor/model_params.pt')
+    predictor = StereoPredictor('ultralytics/yolov5', 'perc22a/predictors/StereoPredictor/model_params.pt')
     dl = DataLoader("data/raw/track-testing-09-29")
     # print(dl[0]['xyz_image'])
 
