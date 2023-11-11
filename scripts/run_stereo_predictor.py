431ed2ab84a00342715ebb3e4acb4f50ecc63496
Emiliano Garcia <60363956+EmilianoGarciaLopez@users.noreply.github.com>
2023-11-11 12:09:49 -0500

    added profiling to stereo_predictor

--- scripts/run_stereo_predictor.py
+++ scripts/run_stereo_predictor.py
@@ -1,9 +1,12 @@
-import sys
+import cProfile  # Import cProfile for profiling
 import os
+import pstats  # Import pstats to format and save the profile statistics
+import sys
+
 import numpy as np
 
-from perc22a.predictors.stereo.StereoPredictor import StereoPredictor
 from perc22a.data.utils.dataloader import DataLoader
+from perc22a.predictors.stereo.StereoPredictor import StereoPredictor
 
 
 def main():
@@ -19,4 +22,16 @@ def main():
 
 
 if __name__ == "__main__":
+    profiler = cProfile.Profile()
+    profiler.enable()
+
     main()
+
+    profiler.disable()  # Stop profiling
+    stats = pstats.Stats(profiler).sort_stats("cumtime")  # Format the stats
+    stats.dump_stats("stereo_profile_results.prof")  # Save stats to a file
+
+    # Optional: You can print the stats to the console as well
+    # stats.print_stats()
+
+#! run 'snakeviz stereo_profile_results.prof' in the terminal
