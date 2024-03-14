11e00eaaa9d430274a87bd3990217990302f247e
Deep Patel <dmpatel@andrew.cmu.edu>
2024-03-14 18:58:00 -0400

    Added required pipelines into NaiveMerger

--- perc22a/mergers/NaiveMerger.py
+++ perc22a/mergers/NaiveMerger.py
@@ -1,6 +1,7 @@
 '''Naive Merger
 
-sufficient() -> true iff non-zero number of cones have been added
+sufficient() -> true iff non-zero number of cones have been added 
+    and required pipelines are obtained
 merge() -> returns all cones added since last reset() (doesn't remove dups)
 '''
 
@@ -8,9 +9,13 @@ from perc22a.mergers.MergerInterface import Merger
 from perc22a.predictors.utils.cones import Cones
 from perc22a.mergers.PipelineType import PipelineType
 
+from types import List
+
 class NaiveMerger(Merger):
 
-    def __init__(self):
+    def __init__(self, required_pipelines: List[PipelineType] = []):
+        self.required_pipelines_set = set(required_pipelines)
+
         self.reset() 
 
         return
@@ -18,17 +23,21 @@ class NaiveMerger(Merger):
     def reset(self):
         self.total_cones = 0
         self.pipeline_cones = {p: Cones() for p in PipelineType}
+        self.seen_pipelines_set = set()
 
         return
 
     def add(self, cones: Cones, pipeline: PipelineType):
         self.total_cones += len(cones)
         self.pipeline_cones[pipeline].add_cones(cones)
+        self.seen_pipelines_set.add(pipeline)
 
         return
 
     def sufficient(self) -> bool:
-        return len(self.pipeline_cones) > 0
+        non_zero_cones = len(self.pipeline_cones) > 0
+        seen_required_cones = self.required_pipelines_set.issubset(self.seen_pipelines_set)
+        return non_zero_cones and seen_required_cones
 
     def merge(self) -> Cones:
         all_cones = Cones()
