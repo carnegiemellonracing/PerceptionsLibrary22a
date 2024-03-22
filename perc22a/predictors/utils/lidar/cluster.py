bfb0d1a66652ee760122b35036f350909ee4bbf2
ChipTheCincoze <geoffthejetson@gmail.com>
2024-03-22 01:31:18 -0400

    removing cuml

--- perc22a/predictors/utils/lidar/cluster.py
+++ perc22a/predictors/utils/lidar/cluster.py
@@ -12,12 +12,7 @@ NOTE: pipeline functions must be registered in the bottom of the file
 import torch
 import perc22a.predictors.utils.lidar.visualization as vis
 
-if torch.cuda.is_available():
-    print("[cluster.py] using GPU-accelerated clustering")
-    from cuml import cluster
-else:
-    print("[cluster.py] using CPU-only clustering")
-    from sklearn import cluster
+from sklearn import cluster
 
 import math
 import time
