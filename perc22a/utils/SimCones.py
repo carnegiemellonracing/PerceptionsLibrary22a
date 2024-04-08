16016cbcdfade6861dbc4e104a31eb6a8599b51b
Deep Patel <dmpatel@andrew.cmu.edu>
2024-04-08 11:55:08 -0400

    moving arguments to constructor

--- perc22a/utils/SimCones.py
+++ perc22a/utils/SimCones.py
@@ -4,24 +4,34 @@ from perc22a.predictors.utils.cones import Cones
 import numpy as np
 import time
 
-V_START = 2 # starting y-value of closest row of cones from the car
-V_SPACE = 3 # spacing between rows of cones
-N_ROWS = 4  # number of rows of cones
-WIDTH = 4.5 # spacing betweeen blue and yellow cone in a single row
-
-PERIOD = 10 # seconds it takes for a single cycle of cone motion to happen
-MAG = 1     # amount of spacing in meters that cones oscillate
-SCALE = 0.5   # add MAG * SCALE to magnitude for row
 
 class SimCones:
 
-    def __init__(self):
+    def __init__(self, v_start=2, v_space=3, n_rows=4, width=4.5, period=10, mag=1, scale=0.5):
+        '''
+        Arguments:
+            v_start (float):    starting y-value of closest row of cones from the car
+            v_space (float):    spacing between rows of cones
+            n_rows (int):       number of rows of cones
+            width (float):      spacing betweeen blue and yellow cone in a single row
+            period (float):     seconds it takes for a single cycle of cone motion to happen
+            mag (float):        amount of spacing in meters that cones oscillate
+            scale (float):      add MAG * SCALE to magnitude for row
+        '''
+
+        self.v_start = v_start
+        self.v_space = v_space
+        self.n_rows = n_rows
+        self.width = width
+        self.period = period
+        self.mag = mag
+        self.scale = scale
 
         self.vis = Vis2D()
 
-        vs = ((np.arange(0, N_ROWS) * V_SPACE) + V_START).reshape((N_ROWS, 1))
-        orig_blue = np.concatenate([np.full((N_ROWS, 1), -WIDTH), vs, np.full((N_ROWS, 1), 0)], axis=1)
-        orig_yellow = np.concatenate([np.full((N_ROWS, 1), WIDTH), vs, np.full((N_ROWS, 1), 0)], axis=1)
+        vs = ((np.arange(0, self.n_rows) * self.v_space) + self.v_start).reshape((self.n_rows, 1))
+        orig_blue = np.concatenate([np.full((self.n_rows, 1), -self.width), vs, np.full((self.n_rows, 1), 0)], axis=1)
+        orig_yellow = np.concatenate([np.full((self.n_rows, 1), self.width), vs, np.full((self.n_rows, 1), 0)], axis=1)
         self.s = time.time()
 
         self.orig_blue = orig_blue.astype(np.float64)
@@ -30,15 +40,18 @@ class SimCones:
         return
 
     def get_cones(self):
+        '''
+        Returns: Cones() data type representing cones at that time
+        '''
 
         delta = time.time() - self.s
 
         blue = np.array(self.orig_blue)
         yellow = np.array(self.orig_yellow)
 
-        mag = MAG * (SCALE * np.arange(0, N_ROWS) + 1)
-        blue[:, 0] += mag * np.cos((delta / PERIOD) * (np.pi * 2))
-        yellow[:, 0] += mag * np.cos((delta / PERIOD) * (np.pi * 2))
+        mag = self.mag * (self.scale * np.arange(0, self.n_rows) + 1)
+        blue[:, 0] += mag * np.cos((delta / self.period) * (np.pi * 2))
+        yellow[:, 0] += mag * np.cos((delta / self.period) * (np.pi * 2))
 
         
         cones = Cones.from_numpy(blue, yellow, np.empty((0,3)))
