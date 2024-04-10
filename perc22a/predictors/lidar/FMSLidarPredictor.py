eccb5af7071da4abe4b3c1b5c70f759b5d0d8662
Deep Patel <dmpatel@andrew.cmu.edu>
2024-04-10 14:02:08 -0400

    added FMS lidar predictor

---
+++ perc22a/predictors/lidar/FMSLidarPredictor.py
@@ -0,0 +1,168 @@
+""" LidarPredictor.py
+
+This file contains the implementation of the perceptions predictions algorithm
+that is solely dependent on raw LiDAR point clouds.
+"""
+
+import cProfile
+
+import numpy as np
+np.set_printoptions(threshold=np.inf)
+
+# interface
+from perc22a.predictors.interface.PredictorInterface import Predictor
+
+# data datatypes
+from perc22a.data.utils.DataInstance import DataInstance
+from perc22a.data.utils.DataType import DataType
+
+# predict output datatype
+from perc22a.predictors.utils.cones import Cones
+from perc22a.predictors.utils.transform.transform import PoseTransformations
+
+# visualization and core lidar algorithm functions
+from perc22a.predictors.utils.vis.Vis3D import Vis3D
+import perc22a.predictors.utils.lidar.visualization as vis
+import perc22a.predictors.utils.lidar.filter as filter
+import perc22a.predictors.utils.lidar.cluster as cluster
+import perc22a.predictors.utils.lidar.color as color
+
+# timer utilities
+from perc22a.utils.Timer import Timer
+
+# constants
+from perc22a.predictors.lidar.constants import *
+
+# general imports
+import numpy as np
+from typing import List
+
+class FMSLidarPredictor(Predictor):
+    def __init__(self):
+        # self.window = vis.init_visualizer_window()
+        self.sensor_name = "lidar"
+        self.transformer = PoseTransformations()
+        self.timer = Timer()
+        self.vis = Vis3D()
+        return
+
+    def profile_predict(self, data):
+        profiler = cProfile.Profile()
+        profiler.enable()
+        cones = self.predict(data)
+        profiler.disable()
+        return cones, profiler
+
+    def required_data(self):
+        return [DataType.HESAI_POINTCLOUD]
+
+    def _transform_points(self, points):
+        points = points[:, :3]
+        points = points[:, [1, 0, 2]]
+        points[:, 0] = -points[:, 0]
+
+        return points
+
+    def predict(self, data) -> Cones:
+        if DEBUG_TIME: self.timer.start("predict")
+        if DEBUG_TIME: self.timer.start("\tinit-process")
+
+        # coordinate frame points to perceptions coordinates system
+        points = data[DataType.HESAI_POINTCLOUD]
+        points = points[~np.any(points == 0, axis=1)]
+        points = points[~np.any(np.isnan(points), axis=-1)]
+        points = points[:, :3]
+        points = self._transform_points(points)
+        self.points = points
+
+        # transfer to origin of car
+        points = self.transformer.to_origin(self.sensor_name, points, inverse=False)
+
+        if DEBUG_TIME: self.timer.end("\tinit-process")
+        if DEBUG_TIME: self.timer.start("\tfilter")
+        if DEBUG_TIME: self.timer.start("\t\tfov-range")
+
+        points_ground_plane = filter.fov_range(
+            points, 
+            fov=180, 
+            minradius=0, 
+            maxradius=INIT_PC_MAX_RADIUS
+        )
+        
+        if DEBUG_TIME: self.timer.end("\t\tfov-range")
+        if DEBUG_TIME: self.timer.start("\t\tground-removal")
+
+        points_filtered_ground = filter.GraceAndConrad(
+            points_ground_plane, 
+            points_ground_plane, 
+            SMART_GROUND_FILTER_SLICE_RADIANS, 
+            SMART_GROUND_FILTER_SLICE_BINS, 
+            SMART_GROUND_FILTER_HEIGHT_THRESHOLD
+        )
+       
+        if DEBUG_TIME: self.timer.end("\t\tground-removal")
+        if DEBUG_TIME: self.timer.start("\t\tnaive-plane-fit")
+
+        _, _, ground_planevals = filter.plane_fit(
+            points,
+            points_ground_plane,
+            return_mask=True,
+            boxdim=NAIVE_PLANE_FIT_BOX_DIM,
+            height_threshold=NAIVE_PLANE_FIT_HEIGHT_THRESHOLD,
+        )
+
+        if DEBUG_TIME: self.timer.end("\t\tnaive-plane-fit")
+        if DEBUG_TIME: self.timer.start("\t\tvoxel-downsample")
+
+        points_cluster_subset = filter.voxel_downsample(
+            points_filtered_ground, 
+            DOWNSAMPLE_VOXEL_SIZE
+        )
+        self.points_cluster_subset = points_cluster_subset
+
+        if DEBUG_TIME: self.timer.end("\t\tvoxel-downsample")
+        if DEBUG_TIME: self.timer.end("\tfilter")
+        if DEBUG_TIME: self.timer.start("\tcluster")
+
+        # predict cone positions 
+        cone_centers = cluster.predict_cones_z(
+            points_cluster_subset,
+            ground_planevals,
+            height_threshold=MAX_CLUSTER_HEIGHT_THRESHOLD,
+        )
+
+        if DEBUG_TIME: self.timer.end("\tcluster")
+        if DEBUG_TIME: self.timer.start("\tcoloring")
+
+        # color cones and correct them
+        cone_output, cone_centers, cone_colors = color.color_cones(cone_centers)
+        cone_output = cluster.correct_clusters(cone_output)
+
+        # create a Cones object to return
+        cones = Cones()
+        for i in range(cone_output.shape[0]):
+            x, y, c = cone_output[i, :]
+            z = cone_centers[i, 2]
+            if c == 1:
+                cones.add_yellow_cone(x, y, z)
+            elif c == 0:
+                cones.add_blue_cone(x, y, z)
+
+        if DEBUG_TIME: self.timer.end("\tcoloring")
+        if DEBUG_TIME: self.timer.start("\ttransform")
+
+        cones = self.transformer.transform_cones(self.sensor_name, cones)
+        self.cones = cones
+
+        if DEBUG_TIME: self.timer.end("\ttransform")
+        if DEBUG_TIME: self.timer.end("predict")
+        return cones
+
+
+    def display(self):
+
+        self.vis.set_points(self.points_cluster_subset)
+        self.vis.set_cones(self.cones)
+        self.vis.update()
+
+        return
