8d907d74b571dfb2f50815727617eac4e7e8b220
ChipTheCincoze <geoffthejetson@gmail.com>
2024-04-21 22:05:25 -0400

    bug fix from merge conflict

--- perc22a/predictors/utils/ConeState.py
+++ perc22a/predictors/utils/ConeState.py
@@ -242,7 +242,7 @@ class ConeState:
         new_cone_pc_arr = self._cones_to_state_arr(new_cones)
 
         # use icp to get correspondences and set cone state w.r.t curr car pos
-        updated_state_pos, corr = self._icp_transform_and_corr(
+        updated_state_pos, corr = self._transform_and_corr(
             self.cones_state_arr[:, :2], 
             new_cone_pc_arr[:, :2]
         )  
