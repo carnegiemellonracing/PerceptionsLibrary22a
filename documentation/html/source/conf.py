50fca65c4a31cea0d5e758d6c06b2bb38185a8d7
Saket Kulkarni <31022476+SaketKulkarni121@users.noreply.github.com>
2024-06-18 18:59:47 -0400

    Changed Theme

--- documentation/html/source/conf.py
+++ documentation/html/source/conf.py
@@ -17,6 +17,8 @@
 
 # -- Project information -----------------------------------------------------
 
+import sphinx_rtd_theme
+
 project = 'Perceptions Library 22a'
 copyright = '2024, Carnegie Mellon Racing'
 author = 'Carnegie Mellon Racing'
@@ -54,9 +56,25 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': 'UA-XXXXXXX-1',
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': '#2880B9',
+
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
+extensions = [
+    'sphinx_rtd_theme',
+]
